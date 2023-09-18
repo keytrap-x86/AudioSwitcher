66cda685278116d1a9bf59f54bab3ca42a4d0dc9
keytrap-x86 <badniiw@gmail.com>
2023-09-18 19:55:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using CoreAudio;
+
+namespace AudioSwitcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MMDeviceEnumerator deviceEnum = new(Guid.NewGuid());
+
+            var currentDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            foreach (MMDevice device in deviceEnum.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+            {
+                if (currentDevice != null)
+                {
+                    if (device.ID != currentDevice.ID)
+                    {
+                        deviceEnum.SetDefaultAudioEndpoint(device);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
